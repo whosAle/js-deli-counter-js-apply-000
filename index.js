cddfd74538b2dd1b3a09e11bd06c07dcbdc3f545
A Belgrave <abelgrave95@gmail.com>
2019-02-15 02:49:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber (line, name){
+  line.push(name);
+  var position = line.indexOf(name)+1
+  return 'Welcome, ${name}. you are number ${position} in line.'
+}
+
+function nowServing(line){
+  if(!line){
+    return "The object inputted is not an array."
+  } else if (line.length == 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving "+ line.shift()
+  }
+}
+
+function currentLine(line){
+  var reply = "The line is currently:"
+  if (line.length == 0) {
+    return "The line is currently empty."
+  } else {
+    for (name in line) {
+      var position = line.indexOf(name)+1
+      reply += "${position}. ${name}, "
+    }
+  }
+
+  return reply
+}
